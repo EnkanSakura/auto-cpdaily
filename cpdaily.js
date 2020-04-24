6c6653b368363d612fe319bde3f893ebf1299e59
EnkanSakura <EnkanSakura@users.noreply.github.com>
2020-04-24 21:22:12 +0800

    更新，纯数字解锁

--- cpdaily.js
+++ cpdaily.js
@@ -1,21 +1,30 @@
-auto.waitFor();     //等待辅助功能开启
 //常量定义部分
 const CURRENT_TIME = new Date();
 const CURRENT_DATE = CURRENT_TIME.getMonth()+1+"月"+CURRENT_TIME.getDate()+"日";   //获取日期，用于定位当日的表
 //可根据需求自行修改的常量
 const DELAY_TIME = 300;     //每次操作的缓冲时间（根据具体情况可延长或缩短）
+const PASSWORD = "";        //引号内填入密码
 //对应所在地的三项
-const province = "";    //分号内填入省
-const city = "";        //分号内填入市
-const district = "";    //分号内填入区
+var province = "**省";      //引号内填入省
+var city = "**市";          //引号内填入市
+var district = "**区";      //引号内填入区
 
+/*
+--------------------------------
+
+    以下代码非开发请勿随意更改
+
+--------------------------------
+*/
+
+//函数定义及实现部分
 function Do(name)   //填一项，并等待
 {
     while(!click(name));
     sleep(DELAY_TIME);
 }
 
-function Fill()
+function Fill()     //填表
 {
     Do("消息");
     Do("辅导员通知");
@@ -29,6 +38,7 @@ function Fill()
     Do("请选择区/县或者国家");
     Do(district);
     Do("确认");
+    //这里的特殊符号和一个空格" "不能删去
     while(!text(" 正常").findOne().parent().child(1).click());
     Do("点击加载更多");
     while(!text(" <37.3℃").findOne().parent().child(1).click());
@@ -42,8 +52,84 @@ function Fill()
     Do("提交");
 }
 
-toastLog("即将启动今日校园");
+function Shake()
+{
+    for(var i = 0; i < 3; i++)
+    {
+        device.vibrate(300);
+        sleep(600);
+    }
+}
+
+function NumberPassword()    //纯数字密码
+{
+    for(var i = 0; i < PASSWORD.length; i++)
+    {
+        var key = PASSWORD[i].toString();
+        text(key).findOne().parent().click();
+    }
+}
+
+function PicturePassword()  //手势密码
+{
+    toast("图案解锁开发中");
+    toast("请手动解锁");
+    Shake();
+}
+
+function MixedPassword()    //混合密码
+{
+    toast("混合密码解锁开发中");
+    toast("请手动解锁");
+    Shake();
+}
+
+function Unlock()
+{
+    while(!device.isScreenOn())
+    {
+        device.wakeUp();
+        sleep(1000);
+        gesture(500, [200,960], [880,960]);
+        sleep(300);
+    }
+    if(device.release >= 7)
+    {
+        
+        if(textContains("输入密码").findOnce())
+        {
+            if(text("ABC").findOne())
+            {
+                NumberPassword();
+            }
+            else
+            {
+                MixedPassword();
+            }
+            sleep(1000);
+        }
+        else if(textContains("绘制").findOne())
+        {
+            PicturePassword();
+        }
+    }
+    else
+    {
+        toast("系统版本过低，请手动解锁");
+        Shake();
+    }
+}
+
+//脚本执行部分
+auto.waitFor();     //等待辅助功能开启
+setScreenMetrics(1080, 1920);
+
+sleep(1000);
+Unlock();   //唤醒屏幕，解锁
+
+toast("即将启动今日校园");  //启动APP
 sleep(2000);
-launch("com.wisedu.apdaily");
+launch("com.wisedu.cpdaily");
 sleep(5000);
-Fill();
+
+// Fill();     //填表
