79af3f937fea31352ae44be0dcfab56923945fe4
EnkanSakura <EnkanSakura@users.noreply.github.com>
2020-05-23 22:01:17 +0800

    1.2.4 update

--- cpdaily.js
+++ cpdaily.js
@@ -5,6 +5,8 @@ const CURRENT_DATE = (CURRENT_TIME.getMonth()+1)+"月"
 //可根据需求自行修改的常量
 const DELAY_TIME = 300;     //每次操作的缓冲时间（根据具体情况可延长或缩短）
 const PASSWORD = "";        //引号内填入密码
+const HAVE_CODE = true;     //是否拥有健康码，填true或false
+const CODE_COLOR = "绿";    //码的颜色，填绿、黄、红，没有可忽略该项
 //对应所在地的三项
 var province = "省";        //引号内填入省
 var city = "市";            //引号内填入市
@@ -23,34 +25,6 @@ function Do(name)   //填一项，并等待
 {
     while(!click(name));
     sleep(DELAY_TIME);
-}
-
-function Fill()     //填表
-{
-    Do("消息");
-    Do("辅导员通知");
-    Do("未填写 >");
-    Do("请选择省或海外");
-    Do(province);
-    Do("确认");
-    Do("请选择市或洲");
-    Do(city);
-    Do("确认");
-    Do("请选择区/县或者国家");
-    Do(district);
-    Do("确认");
-    
-    while(!textContains("正常").findOne().parent().child(1).click());
-    Do("点击加载更多");
-    while(!textContains("<37.3℃").findOne().parent().child(1).click());
-    var bottom = textContains("否").find();
-    for(var i = 0; i < bottom.size(); i++)
-    {
-        bottom[i].parent().child(1).click();
-    }
-    Do("提交给辅导员");
-    sleep(1000);
-    Do("提交");
 }
 
 function Shake()
@@ -113,6 +87,46 @@ function Unlock()
         toast("系统版本过低，请手动解锁");
         Shake();
     }
+}
+
+
+function Fill()     //填表
+{
+    Do("消息");
+    Do("辅导员通知");
+    Do("未填写");
+    Do("请选择省或海外");
+    Do(province);
+    Do("确认");
+    Do("请选择市或洲");
+    Do(city);
+    Do("确认");
+    Do("请选择区/县或者国家");
+    Do(district);
+    Do("确认");
+    
+    while(!textContains("正常").findOne().parent().child(1).click());
+    Do("点击加载更多");
+    while(!textContains("37.3℃").find()[1].parent().child(1).click());
+    var bottom = textContains("否").find();
+    var b_end = bottom.size() - 1;
+    for(var i = 0; i < b_end; i++)
+    {
+        bottom[i].parent().child(1).click();
+    }
+    if(HAVE_CODE)
+    {
+        bottom[b_end].parent().parent().child(0).child(1).click();
+        while(!textContains(CODE_COLOR).findOne().parent().child(1).click());
+    }
+    else
+    {
+        bottom[b_end].parent().parent().child(0).child(0).click();
+        while(!textContains("未申请").findOne().parent().child(1).click());
+    }
+    Do("提交给辅导员");
+    sleep(1000);
+    Do("提交");
 }
 
 //脚本执行部分
